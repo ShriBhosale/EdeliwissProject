ed5502331c5ec13ae80def120e491794d8da1592
Mauri mohite <bhosaleshri@gmail.com>
2020-08-14 19:09:22 +0530

    solve confict

--- src/main/java/com/shreeya/amazone/FactoryExprement.java
+++ src/main/java/com/shreeya/amazone/FactoryExprement.java
@@ -4,9 +4,6 @@ import org.testng.annotations.Factory;
 
 public class FactoryExprement {
 	
-	@Factory
-	public Object[] myfactory() {
-		return new Object [] {new TestFactory1("Women cloths"),new TestFactory2("men cloths")};
-	}
+	
 
 }
